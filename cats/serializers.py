b4842a2b9b144499738f9e2b8f6177d794758184
crkubiak <crkubiak@gmail.com>
2019-04-03 16:32:42 -0500

    serializer correction

--- cats/serializers.py
+++ cats/serializers.py
@@ -4,4 +4,4 @@ from .models import Cat
 class CatSerializer(serializers.ModelSerializer):
   class Meta:
     model = Cat
-    fields = ['project_id','cat_name', 'cat_age', 'cat_sex', 'cat_size', 'cat_color', 'cat_coat', 'cat_description', 'cat_fix_spay', 'cat_declawed', 'created_at', 'updated_at']
+    fields = ['project','cat_name', 'cat_age', 'cat_sex', 'cat_size', 'cat_color', 'cat_coat', 'cat_description', 'cat_fix_spay', 'cat_declawed', 'created_at', 'updated_at']
