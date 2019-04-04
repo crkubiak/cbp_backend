76c72955cea9edc8240071e787e7acca61b95e54
crkubiak <crkubiak@gmail.com>
2019-04-03 22:18:49 -0500

    create super user

--- cats/serializers.py
+++ cats/serializers.py
@@ -5,3 +5,5 @@ class CatSerializer(serializers.ModelSerializer):
   class Meta:
     model = Cat
     fields = ['project','cat_name', 'cat_age', 'cat_sex', 'cat_size', 'cat_color', 'cat_coat', 'cat_description', 'cat_fix_spay', 'cat_declawed', 'created_at', 'updated_at']
+
+    # test
