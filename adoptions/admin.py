ec073353727d160b2b6139ebb845a2f5e109a776
crkubiak <crkubiak@gmail.com>
2019-04-03 22:42:22 -0500

    corrected admin panels

--- adoptions/admin.py
+++ adoptions/admin.py
@@ -1,3 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin 
+from .models import Adoption
 
-# Register your models here.
+admin.site.register(Adoption)
--- cbp_backend/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin # brings in the admin ability
-from .models import Post # brings in your Post model from the models.py file
-
-admin.site.register(Post) # registers Post with your admin ability
