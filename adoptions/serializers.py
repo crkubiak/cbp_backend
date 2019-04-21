319ae3798453699eacb1da3ca56171b6d288bfb8
crkubiak <crkubiak@gmail.com>
2019-04-21 14:18:49 -0500

    updated id field on serializer

--- adoptions/serializers.py
+++ adoptions/serializers.py
@@ -4,4 +4,4 @@ from .models import Adoption
 class AdoptionSerializer(serializers.ModelSerializer):
   class Meta:
     model = Adoption
-    fields = ['cat', 'adoption_first_name', 'adoption_last_name', 'adoption_address', 'adoption_city', 'adoption_state', 'adoption_zip', 'adoption_phone', 'adoption_email', 'adoption_housing', 'adoption_indoor_outdoor', 'adoption_move', 'adoption_move', 'adoption_why', 'created_at', 'updated_at']
+    fields = ['id','cat', 'adoption_first_name', 'adoption_last_name', 'adoption_address', 'adoption_city', 'adoption_state', 'adoption_zip', 'adoption_phone', 'adoption_email', 'adoption_housing', 'adoption_indoor_outdoor', 'adoption_move', 'adoption_move', 'adoption_why', 'created_at', 'updated_at']
--- cats/serializers.py
+++ cats/serializers.py
@@ -6,4 +6,3 @@ class CatSerializer(serializers.ModelSerializer):
     model = Cat
     fields = ['project','cat_name', 'cat_age', 'cat_sex', 'cat_size', 'cat_color', 'cat_coat', 'cat_description', 'cat_fix_spay', 'cat_declawed', 'created_at', 'updated_at']
 
-    # test
